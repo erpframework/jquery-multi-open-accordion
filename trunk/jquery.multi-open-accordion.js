51749812e403418349436545046993b6b8e21625
anas.nakawa@gmail.com <anas.nakawa@gmail.com@bb0939a7-e449-d8fa-3f2a-b49a88787c7a>
2011-02-18 17:56:16 +0000

    adding active option

--- trunk/jquery.multi-open-accordion.js
+++ trunk/jquery.multi-open-accordion.js
@@ -11,6 +11,7 @@
  		multiAccordion: function(options) {
 			// TODO: no defaults yet
 			var defaults = {
+				active: 0
 			}
 			var options =  $.extend(defaults, options);
     		return this.each(function() {
@@ -18,29 +19,25 @@
 				var $h3 = $this.children('h3');
 				var $div = $this.children('div');
 				$this.addClass('ui-accordion ui-widget ui-helper-reset ui-accordion-icons');
-				$h3.each(function(){
-					$(this).addClass('ui-accordion-header ui-helper-reset ui-state-default ui-corner-all').prepend('<span class="ui-icon ui-icon-triangle-1-e"></span>')
+				$h3.each(function(index){
+					var $this = $(this);
+					$this.addClass('ui-accordion-header ui-helper-reset ui-state-default ui-corner-all').prepend('<span class="ui-icon ui-icon-triangle-1-e"></span>');
+					if(isActive(index)) {
+						showTab($this)
+					} else {
+						hideTab($this)
+					}
 				});
-				$this.children('div').each(function(){
-					$(this).addClass('ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom');
+				$this.children('div').each(function(index){
+					var $this = $(this);
+					$this.addClass('ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom');
 				});
 				$h3.click(function(){
-					var $this = $(this);
-					var $span = $this.children('span.ui-icon');
-					var $div = $this.next();
-					
+					var $this = $(this);					
 					if ($this.hasClass('ui-state-default')) {
-						$this.removeClass('ui-state-default ui-corner-all').addClass('ui-state-active ui-corner-top');
-						$span.removeClass('ui-icon-triangle-1-e').addClass('ui-icon-triangle-1-s');
-						$div.slideDown('fast', function(){
-							$div.addClass('ui-accordion-content-active');
-						});
+						showTab($this);
 					} else {
-						$this.removeClass('ui-state-active ui-corner-top').addClass('ui-state-default ui-corner-all');
-						$span.removeClass('ui-icon-triangle-1-s').addClass('ui-icon-triangle-1-e');
-						$div.slideUp('fast', function(){
-							$div.removeClass('ui-accordion-content-active');
-						});
+						hideTab($this);
 					}
 				});
 				$h3.hover(
@@ -51,6 +48,43 @@
 					}
 				);
     		});
+			
+			function showTab($this) {
+				var $span = $this.children('span.ui-icon');
+				var $div = $this.next();
+				$this.removeClass('ui-state-default ui-corner-all').addClass('ui-state-active ui-corner-top');
+				$span.removeClass('ui-icon-triangle-1-e').addClass('ui-icon-triangle-1-s');
+				$div.slideDown('fast', function(){
+					$div.addClass('ui-accordion-content-active');
+				});
+			}
+			
+			function hideTab($this) {
+				var $span = $this.children('span.ui-icon');
+				var $div = $this.next();
+				$this.removeClass('ui-state-active ui-corner-top').addClass('ui-state-default ui-corner-all');
+				$span.removeClass('ui-icon-triangle-1-s').addClass('ui-icon-triangle-1-e');
+				$div.slideUp('fast', function(){
+					$div.removeClass('ui-accordion-content-active');
+				});
+			}
+			
+			function isActive(num) {
+				// if array
+				if(typeof options.active == "boolean" && !options.active) {
+					return false;
+				} else {
+					if(options.active.length != undefined) {
+						for (var i = 0; i < options.active.length ; i++) {
+							if(options.active[i] == num)
+								return true;
+						}
+					} else {
+						return options.active == num;
+					}
+				}
+				return false;
+			}
     	}
 	});
 })(jQuery);
